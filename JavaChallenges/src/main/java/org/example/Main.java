a79f59d65a920c056a2292bcca3cb56c47b7faf5
PavelJakoube <jakoubep@seznam.cz>
2023-10-31 14:00:52 +0100

    unique_in_order

--- JavaChallenges/src/main/java/org/example/Main.java
+++ JavaChallenges/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Scramblies sc = new Scramblies();
 
+        System.out.println(sc.scramble("scriptingjava","javascript"));
     }
 }
