ba764a555d8c8ad760943e88435b8171205ae496
PavelJakoube <jakoubep@seznam.cz>
2023-11-08 11:14:28 +0100

    fibonacciCache

--- JavaChallenges/src/main/java/org/example/Main.java
+++ JavaChallenges/src/main/java/org/example/Main.java
@@ -2,8 +2,5 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Scramblies sc = new Scramblies();
-
-        System.out.println(sc.scramble("scriptingjava","javascript"));
     }
 }
